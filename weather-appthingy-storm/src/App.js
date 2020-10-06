d572b046afb7bcfd4dd8ba5fa974729cc4c6e284
unknown <djfodsjvclkdshvcfijs@lkdlkposaksadksadkad.com>
2020-10-06 18:15:25 +0300

    Tein sääjuttuja

--- weather-appthingy-storm/src/App.js
+++ weather-appthingy-storm/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import Weather from './Weather'
 function App() {
   return (
-    <div className="App">
-    <header className="App-header">
+    <div className='App'>
+    <header className='App-header'>
       Sademyrsky
     </header>
+    <Weather/>
       Moi, mitä kuuluu?
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ weather-appthingy-storm/src/Weather.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="Weather">
+        <div className="Weather_focused">
+        <div className="Weather_day">ma</div>
+        <div className="Weather_day">12:00</div>
+        <div className="Weather_temp">14C°</div>
+        <img src='http://openweathermap.org/img/wn/09d.png'
+        </div>
+        test
+      </div>
+    )
+  }
+}
+
+export default Weather
