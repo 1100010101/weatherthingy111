f9de454d35c48da735c106cb3ab47ecee164c6a6
Noel <kangasnoel1@gmail.com>
2020-12-08 17:50:09 +0200

    Updates

---
+++ precache-manifest.68301f8b1c9382749d18a2405794327c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7506d86f6057646aedcf84ce1ab13d13",
+    "url": "/%3Cweatherthingy111%3E/index.html"
+  },
+  {
+    "revision": "57ac0d1f9f9c923dc08c",
+    "url": "/%3Cweatherthingy111%3E/static/css/main.87ec80de.chunk.css"
+  },
+  {
+    "revision": "55ece659e4ec4234ada9",
+    "url": "/%3Cweatherthingy111%3E/static/js/2.a197c529.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cweatherthingy111%3E/static/js/2.a197c529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ac0d1f9f9c923dc08c",
+    "url": "/%3Cweatherthingy111%3E/static/js/main.a15416c0.chunk.js"
+  },
+  {
+    "revision": "80081d13936428106f86",
+    "url": "/%3Cweatherthingy111%3E/static/js/runtime-main.6e479426.js"
+  }
+]);
